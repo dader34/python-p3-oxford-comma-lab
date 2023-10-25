1d0bdc56018e7bea0d4e52d63132e610c5723177
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-25 15:10:34 -0700

    Passed all tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,19 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(it):
+    items = [str(item) for item in it]
+    if(len(items) == 1):
+        return str(items[0])
+    elif(len(items) == 2):
+        return f'{str(items[0])} and {str(items[1])}'
+    elif(len(items) == 3):
+        return f'{str(items[0])}, {str(items[1])}, and {str(items[2])}'
+    else:
+        for item in items:
+            if(items.index(item) %2 == 0 and (items.index(item) != len(items) -1)):
+                items.insert(items.index(item)+1,", ")
+            elif(items.index(item) == len(items)-1):
+                items.insert(items.index(item),"and ")
+                break
+    return(''.join(items))
+
+
+print(oxford_comma([1,2,3,4,5]))
